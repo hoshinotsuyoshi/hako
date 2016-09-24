1a2f93d6e6fc023a357d35ed93426d24a091d847
Kohei Suzuki <eagletmt@gmail.com>
2016-09-24 16:39:47 +0900

    WIP: Schema

---
+++ lib/hako/structure.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require 'hako/schema'
+require 'hako/schema'
+
+module Hako
+  class Structure
+    def initialize(schema)
+      @schema = schema
+    end
+  end
+end
